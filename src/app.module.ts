afcfb127d8ea4b7f5487170694c0fc25e23699bc
1O16 <youiiyou1253@gmail.com>
2022-11-14 11:32:46 +0900

    Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 // 데코레이터 - 클래스에 함수 기능을 추가(클래스 위의 함수), 데코레이터와 함수 사이에 공백이 있으면 안됌
 @Module({
-  imports: [],
-  controllers: [MoviesController], // url을 가져와서 함수를 실행시킴
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // url을 가져와서 함수를 실행시킴
+  providers: [],
 })
 export class AppModule {}
