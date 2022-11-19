ef1211dc49650f56d63c5c0ac470955dc40f6475
[1O16] <youiiyou1253@gmail.com>
2022-11-19 22:51:43 +0900

    prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
